c9529428b9e095b42cb5238aa6fa57a4d476f522
ric <ricardo.najera@usaa.com>
2019-07-02 14:07:03 -0500

    fix: cleaning up coverage packages

--- src/components/PackageSummary.js
+++ src/components/PackageSummary.js
@@ -1,31 +1,27 @@
 import React from 'react';
 import CardDeck from "react-bootstrap/CardDeck";
 import Container from "react-bootstrap/Container";
-import {connect} from "react-redux";
-import {selectPackage} from "../actions";
 import Form from 'react-bootstrap/Form'
-import DetailCard from "./DetialCard";
+import PackageCard from "./PackageCard";
 import CoverageSelection from "./CoverageSelector";
 
-const PackageSummary = props => {
-    const handleClick = event => props.selectPackage(event.target.id);
-    const handleButton = id => id === props.selectPackage ? 'primary' : 'outline-primary';
-
-    return (
+const PackageSummary = () => (
         <Container fluid={true}>
             <CardDeck className='relaxed'>
-                <DetailCard handleClick={handleClick} handleButton={handleButton} />
-                <DetailCard handleClick={handleClick} handleButton={handleButton} />
-                <DetailCard handleClick={handleClick} handleButton={handleButton} />
+                <PackageCard id='standard' />
+                <PackageCard id='premium' />
+                <PackageCard id='minimum' />
             </CardDeck>
             <Form>
                 <Form.Row>
+                    <CoverageSelection />
+                    <CoverageSelection />
+                    <CoverageSelection />
                     <CoverageSelection />
                     <CoverageSelection />
                 </Form.Row>
             </Form>
         </Container>
     );
-};
 
-export default connect(state => ({ selectedPackage: state.selectedPackage }), { selectPackage })(PackageSummary);
+export default PackageSummary;
