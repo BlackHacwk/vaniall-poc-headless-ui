8b1b6d6ba675dddfcc41e85b0e250805e154bb13
ric <ricardo.najera@usaa.com>
2019-07-02 14:49:30 -0500

    fix: cleaning up coverage packages 2

--- src/components/CoverageSelector.js
+++ src/components/CoverageSelector.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 
-const CoverageSelection = props =>
+const CoverageSelection = () =>
 <Form.Group as={Col} controlId="formGridState">
     <Form.Label>Bodily Injury</Form.Label>
     <Form.Control as="select">
-        <option>Choose...</option>
-        <option>...</option>
+        <option>option 1</option>
+        <option>option 2</option>
+        <option>option 3</option>
     </Form.Control>
 </Form.Group>;
 
