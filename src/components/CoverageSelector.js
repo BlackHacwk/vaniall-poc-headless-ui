61d31db58fe0161ca457cccc280f4141ad2af2d4
ric <ricardo.najera@usaa.com>
2019-07-15 14:56:54 -0500

    added dynamic coverage selectors based on package selection

--- src/components/CoverageSelector.js
+++ src/components/CoverageSelector.js
@@ -4,19 +4,17 @@ import Form from "react-bootstrap/Form";
 import {connect} from "react-redux";
 import {gbbRes} from "../__mocks__";
 
-const CoverageSelection = props =>{
-        console.log(props);
-        return (
-            <Form.Row>
-                {gbbRes.result.lobData.personalAuto.offerings.filter(offer =>
-                        offer.branchCode === props.selectedPackage)[0].coverages.lineCoverages.map(coverage =>
-                        coverage.terms.length > 0
-                        ? <Form.Group as={Col} controlId="formGridState"><Form.Label>{coverage.name}</Form.Label><Form.Control as="select">{coverage.terms[0].options.map(option=> <option>{option.name}</option>)}</Form.Control></Form.Group>
-                        : null
-                    )}
-            </Form.Row>
-        );
-
-};
+const CoverageSelection = props =>
+        <Form.Row>
+            {gbbRes.result.lobData.personalAuto.offerings.filter(offer =>
+                offer.branchCode === props.selectedPackage)[0].coverages.lineCoverages.map(coverage =>
+                coverage.terms.length > 0
+                ? <Form.Group as={Col} controlId="formGridState">
+                        <Form.Label>{coverage.name}</Form.Label>
+                        <Form.Control as="select">{coverage.terms[0].options.map(option=> <option>{option.name}</option>)}</Form.Control>
+                  </Form.Group>
+                : null
+            )}
+        </Form.Row>;
 
 export default connect(state=> ({selectedPackage: state.selectedPackage}), null)(CoverageSelection);
